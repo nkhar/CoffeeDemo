28a72f9e5d53ac8c4593ce9cc5c2a02f57b80553
Nika <Nikoloz.Kharshiladze.2014@live.rhul.ac.uk>
2018-03-26 17:08:50 +0400

    Added Adapter to display PreOrdered accounts in PreOrder item of Navigation Drawer. It uses fragment_machine for layout.

---
+++ app/src/main/java/android/apex/ge/coffee/Fragments/MyPreOrderAccountsRecyclerViewAdapter.java
@@ -0,0 +1,101 @@
+package android.apex.ge.coffee.Fragments;
+
+import android.apex.ge.coffee.R;
+import android.apex.ge.coffee.Retrofit.Model.AccountInfo;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.List;
+
+/**
+ * Created by Nika on 26/03/2018.
+ * This class is an adapter to display list of PreOrderAccounts
+ */
+
+public class MyPreOrderAccountsRecyclerViewAdapter extends RecyclerViewListAdapter<MyPreOrderAccountsRecyclerViewAdapter.ViewHolder, AccountInfo>{
+
+    protected final String LOG_TAG = "MyPreOrderAccRecAdapter";
+
+    public MyPreOrderAccountsRecyclerViewAdapter(List<AccountInfo> items) {
+        super(items);
+        Log.d(LOG_TAG, "We are in MyPreOrderAccountsRecyclerViewAdapter   constructor   ");
+    }
+
+    @NonNull
+    @Override
+    public MyPreOrderAccountsRecyclerViewAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.fragment_pre_order_account_item, parent, false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(final ViewHolder holder, final AccountInfo value) {
+        if (value == null) {
+            return;
+        }
+
+        holder.mAcccountInfoResult = value;
+
+        holder.mPreOrderAccountsAccTextView.setText(value.getAcc());
+        holder.mPreOrderAccountsNameTextView.setText(value.getName());
+        holder.mPreOrderAccountsVatTypeTextView.setText(String.valueOf(value.getVatType()));
+      /*  holder.mRawMaterialsBCodeTextView.setText(value.getAccWithName());
+        holder.mRawMaterialsInCodeTextView.setText(value.getAddress());
+        holder.mRawMaterialsPackCountTextView.setText(value.getsN());
+        holder.mRawMaterialsRCountTextView.setText(value.getWarehouseAddress());*/
+
+        final ILibObjectCrud listener = getmListener();
+        if (listener != null) {
+            holder.mView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    listener.onClick(value);
+                }
+            });
+            holder.mView.setOnLongClickListener(new View.OnLongClickListener() {
+                @Override
+                public boolean onLongClick(View v) {
+                    listener.onLongClick(value);
+                    return true;
+                }
+            });
+        }
+    }
+
+
+    // Provide a direct reference to each of the views within a data item
+// Used to cache the views within the item layout for fast access
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        // Your holder should contain a member variable
+        // for any view that will be set as you render a row
+        public final View mView;
+        public final TextView mPreOrderAccountsAccTextView;
+        public final TextView mPreOrderAccountsNameTextView;
+        public final TextView mPreOrderAccountsVatTypeTextView;
+       /* public final TextView mRawMaterialsInCodeTextView;
+        public final TextView mRawMaterialsPackCountTextView;
+        public final TextView mRawMaterialsRCountTextView;
+        public final TextView mRawMaterialsVanRCountTextView;*/
+        public AccountInfo mAcccountInfoResult;
+
+        public ViewHolder(View itemView) {
+            // Stores the itemView in a public final member variable that can be used
+            // to access the context from any ViewHolder instance.
+            super(itemView);
+            mView = itemView;
+            mPreOrderAccountsAccTextView = itemView.findViewById(R.id.pre_order_account_Acc);
+            mPreOrderAccountsNameTextView = itemView.findViewById(R.id.pre_order_account_Name);
+            mPreOrderAccountsVatTypeTextView = itemView.findViewById(R.id.pre_order_account_VatType);
+/*            mRawMaterialsInCodeTextView = itemView.findViewById(R.id.page_raw_materials_InCode);
+            mRawMaterialsPackCountTextView = itemView.findViewById(R.id.page_raw_materials_PackCount);
+            mRawMaterialsRCountTextView = itemView.findViewById(R.id.page_raw_materials_RCount);
+            mRawMaterialsVanRCountTextView = itemView.findViewById(R.id.page_raw_materials_VanRCount);*/
+        }
+
+    }
+}
--- app/src/main/java/android/apex/ge/coffee/Fragments/PreOrderFragment.java
+++ app/src/main/java/android/apex/ge/coffee/Fragments/PreOrderFragment.java
@@ -1,23 +1,41 @@
 package android.apex.ge.coffee.Fragments;
 
 import android.apex.ge.coffee.R;
+import android.apex.ge.coffee.Retrofit.CoffeeMachineList;
+import android.apex.ge.coffee.Retrofit.CoffeeServiceAPI.CoffeeService;
+import android.apex.ge.coffee.Retrofit.CoffeeServiceAPI.RetrofitClient;
+import android.apex.ge.coffee.Retrofit.Model.AccountInfo;
+import android.apex.ge.coffee.Retrofit.Model.CoffeeMachine;
+import android.apex.ge.coffee.Retrofit.PreOrderAccounts;
 import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.DividerItemDecoration;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.text.method.ScrollingMovementMethod;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
 /**
  * Created by Nika on 26/03/2018.
  * This class should display one of the fragments chosen from navigation drawer.
  */
 
-public class PreOrderFragment extends Fragment{
+public class PreOrderFragment extends Fragment implements ILibObjectCrud{
 
     protected final String LOG_TAG = "PreOrderFragment";
 
@@ -52,9 +70,77 @@ public class PreOrderFragment extends Fragment{
         /*
         Recycler View
          */
+
         Context context = view.getContext();
+        recyclerView = view.findViewById(R.id.coffee_machines_recycler_view);
+        if (mColumnCount <= 1) {
+            recyclerView.setLayoutManager(new LinearLayoutManager(context));
+        } else {
+            recyclerView.setLayoutManager(new GridLayoutManager(context, mColumnCount));
+        }
+
+
+        RecyclerView.ItemDecoration localItemDecoration = new DividerItemDecoration(context, DividerItemDecoration.VERTICAL);
+        recyclerView.addItemDecoration(localItemDecoration);
+
+         /*
+        Floating Action Button
+         */
+        FloatingActionButton fab = view.findViewById(R.id.fab_fragment_machine);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                CoffeeService service = RetrofitClient.getRetrofitClient().create(CoffeeService.class);
+
+                Call<PreOrderAccounts> preOrderAccounts = service.listPreOrderAccounts();
+
+
+
+
+                preOrderAccounts.enqueue(new Callback<PreOrderAccounts>() {
+                    @Override
+                    public void onResponse(Call<PreOrderAccounts> call, Response<PreOrderAccounts> response) {
+                        if(response.isSuccessful()) {
+                            Log.d(LOG_TAG, response.code() + "");
+
+                           /* String displayCoffeeResponse = "";*/
+                            List<AccountInfo> accounts = response.body().getResult();
+                            adapter = new MyPreOrderAccountsRecyclerViewAdapter(accounts);
+                            adapter.setmListener(PreOrderFragment.this);
+                            PreOrderFragment.this.recyclerView.setAdapter(adapter);
+                           /* displayCoffeeResponse += "\n    " + kofe.size() + " \n";*/
+
+                           /* for (CoffeeMachineList.Result coffeeResult : kofe) {
+                                displayCoffeeResponse += coffeeResult.toString();
+//                                Log.d(LOG_TAG, coffeeResult.getAcc().toString());
+                            }*/
+                            textView.setText(adapter.getItemCount() + " \n\n " + accounts.size() + "\n\n");
+                        }else
+                            textView.setText(response.toString());
+                        textView.setMovementMethod(new ScrollingMovementMethod());
+                    }
+
+                    @Override
+                    public void onFailure(Call<PreOrderAccounts> call, Throwable t) {
+                        call.cancel();
+                        textView.setText("Can't Establish a Connection to the Server\n\n" + call.toString() + "\n\n" + t.getStackTrace());
+                    }
+                });
+            }
+        });
 
         return view;
 
+    }
+
+
+    @Override
+    public void onClick(Object value) {
+        Log.d(LOG_TAG, "Something was clicked" + value.toString());
+    }
+
+    @Override
+    public void onLongClick(Object value) {
+        Log.d(LOG_TAG, "Something was longClicked" + value.toString());
     }
 }
