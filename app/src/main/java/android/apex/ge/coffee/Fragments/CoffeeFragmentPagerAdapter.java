7178e44f32ea5b7a1bb94fe7065936612cc71186
Nika <Nikoloz.Kharshiladze.2014@live.rhul.ac.uk>
2018-04-27 19:21:27 +0400

    Translated a few things changed constraints in the layout of activity_login. CoffeeFragmentPagerAdapter uses resources accessed through context to display titles of pages instead of hardcoded values as before.

--- app/src/main/java/android/apex/ge/coffee/Fragments/CoffeeFragmentPagerAdapter.java
+++ app/src/main/java/android/apex/ge/coffee/Fragments/CoffeeFragmentPagerAdapter.java
@@ -1,6 +1,8 @@
 package android.apex.ge.coffee.Fragments;
 
+import android.apex.ge.coffee.R;
 import android.content.Context;
+import android.content.res.Resources;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
@@ -17,12 +19,16 @@ import android.support.v4.app.FragmentPagerAdapter;
 public class CoffeeFragmentPagerAdapter extends FragmentPagerAdapter {
 
     final int PAGE_COUNT = 4;
-    private String tabTitles[] = new String[]{"Sale", "Sale Produced", "Raw Materials", "Money"};
+    private String tabTitles[] = new String[PAGE_COUNT];
     private Context context;
 
     public CoffeeFragmentPagerAdapter(FragmentManager fm, Context context) {
         super(fm);
         this.context = context;
+        tabTitles[0] = this.context.getString(R.string.coffee_machine_sale_page_title);
+        tabTitles[1] = this.context.getString(R.string.coffee_machine_produced_page_title);;
+        tabTitles[2] = this.context.getString(R.string.coffee_machine_raw_materials_page_title);;
+        tabTitles[3] = this.context.getString(R.string.coffee_machine_money_page_title);;
     }
 
     @Override
